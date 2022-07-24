938e1425db6c6e68b244e4b5e24155c9c6390eec
Adam Pearson <adam.pearson94@gmail.com>
2022-07-24 19:24:32 +0100

    Added tasktable component to display headings above the list of tasks

---
+++ src/components/TaskTable.tsx
@@ -0,0 +1,39 @@
+import { Box, Stack, Typography } from "@mui/material";
+import React from "react";
+import TaskList from "./TaskList";
+import { tasks } from '../apiDummy';
+
+const TaskTable = () => {
+    return (
+        <>
+            <Stack direction="column" sx={{ width: "100%" }}>
+                <Stack direction="row" px={2}>
+                    <Typography variant="subtitle1" sx={{ width: "60%" }}>
+                        Task
+                    </Typography>
+                    <Typography
+                        variant="subtitle1"
+                        align="center"
+                        sx={{ width: "10%" }}
+                    >
+                        Status
+                    </Typography>
+                    <Typography
+                        variant="subtitle1"
+                        align="center"
+                        sx={{ width: "10%" }}
+                    >
+                        Priority
+                    </Typography>
+                    <Typography variant="subtitle1" sx={{ width: "15%" }}>
+                        Assignees
+                    </Typography>
+                    <Box sx={{ width: "10%" }}></Box>
+                </Stack>
+                <TaskList tasks={tasks}/>
+            </Stack>
+        </>
+    );
+};
+
+export default TaskTable;
