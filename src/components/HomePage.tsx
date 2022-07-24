393f531b0befa079c54aa57ed84b7e005690b739
Adam Pearson <adam.pearson94@gmail.com>
2022-07-24 19:25:15 +0100

    Added basic grid for homepage layout

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,8 +1,22 @@
+import { Box, Grid, Stack, Typography } from '@mui/material'
 import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <Grid container sx={{ 
+      overflow: 'hidden',
+     }}>
+    <Grid item xs={2} >
+      <Box bgcolor='primary.light' sx={{
+        height: "100vh", width: "100%"
+        }}>Item 1</Box>
+    </Grid>
+    <Grid item xs={10}>
+      <Box bgcolor='secondary.light' sx={{
+        height: "100vh",
+        }}>Item 2</Box>
+    </Grid>
+    </Grid>
   )
 }
 
