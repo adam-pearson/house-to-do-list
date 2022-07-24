551ef96ae01c82f4b7d95733ccfcaa78648fe08b
Adam Pearson <adam.pearson94@gmail.com>
2022-07-24 20:35:52 +0100

    Added table and add task button to homepage

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,23 +1,54 @@
-import { Box, Grid, Stack, Typography } from '@mui/material'
-import React from 'react'
+import { Box, Grid, Stack, Typography } from "@mui/material";
+import React, { useState } from "react";
+import AddTaskButton from "./AddTaskButton";
+import AddTaskForm from "./AddTaskForm";
+import TaskTable from "./TaskTable";
 
 const HomePage = () => {
-  return (
-    <Grid container sx={{ 
-      overflow: 'hidden',
-     }}>
-    <Grid item xs={2} >
-      <Box bgcolor='primary.light' sx={{
-        height: "100vh", width: "100%"
-        }}>Item 1</Box>
-    </Grid>
-    <Grid item xs={10}>
-      <Box bgcolor='secondary.light' sx={{
-        height: "100vh",
-        }}>Item 2</Box>
-    </Grid>
-    </Grid>
-  )
-}
+    const [addTaskFormOpen, setAddTaskFormOpen] = useState(false);
 
-export default HomePage
+    console.log(addTaskFormOpen);
+
+    return (
+        <Grid
+            container
+            sx={{
+                overflow: "hidden",
+            }}
+        >
+            <Grid
+                item
+                xs={0}
+                lg={2}
+                sx={{ display: { xs: "none", lg: "block" } }}
+            >
+                <Box
+                    bgcolor="primary.light"
+                    sx={{
+                        height: "100vh",
+                        width: "100%",
+                    }}
+                >
+                    Item 1
+                </Box>
+            </Grid>
+            <Grid item xs={12} lg={10} px={2} sx={{ backgroundColor: "grey.100" }}>
+                <Box my={2}>
+                    <AddTaskButton
+                        addTaskFormOpen={addTaskFormOpen}
+                        setAddTaskFormOpen={setAddTaskFormOpen}
+                    />
+                </Box>
+                {addTaskFormOpen ?
+                  <Box>
+                    <AddTaskForm />
+                  </Box>
+                  : null
+                }
+                <TaskTable />
+            </Grid>
+        </Grid>
+    );
+};
+
+export default HomePage;
