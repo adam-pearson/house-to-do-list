c5151ffcd433e25ae974a191a39bf8232b4d6391
Adam Pearson <adam.pearson94@gmail.com>
2022-07-25 22:29:20 +0100

    Modified task table to use the MUI grid instead of stack and percentage widths, also set it to pull real task data in
    - Need to get it automatically refreshing task list when saving a new task

--- src/components/TaskTable.tsx
+++ src/components/TaskTable.tsx
@@ -1,38 +1,58 @@
-import { Box, Stack, Typography } from "@mui/material";
-import React from "react";
+import { Box, Grid, Stack, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
 import TaskList from "./TaskList";
-import { tasks } from '../apiDummy';
+import { tasks } from "../apiDummy";
+import apiClient from "../services/apiClient";
 
 const TaskTable = () => {
+
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        apiClient.get('/api/tasks')
+        .then((response: any) => {
+            setTasks(response.data);
+            console.log("PULLED TASKS: ", response);
+        });
+    }, []);
+
     return (
-        <>
-            <Stack direction="column">
-                <Stack direction="row" px={2}>
-                    <Typography variant="subtitle1" sx={{ width: "60%" }}>
+        <Box>
+            <Grid container p={2}>
+                <Grid item xs={8} md={6} lg={6}>
+                    <Typography variant="subtitle1">
                         Task
                     </Typography>
+                </Grid>
+                <Grid item xs={4} md={3} lg={2}>
                     <Typography
                         variant="subtitle1"
-                        align="center"
-                        sx={{ width: "10%" }}
+                        sx={{ textAlign: 'center' }}
                     >
                         Status
                     </Typography>
+                </Grid>
+                <Grid item xs={0} md={2} lg={1} sx={{ display: {xs: 'none', md: 'block' } }}>
                     <Typography
                         variant="subtitle1"
-                        align="center"
-                        sx={{ width: "10%" }}
+                        sx={{ textAlign: 'center' }}
                     >
                         Priority
                     </Typography>
-                    <Typography variant="subtitle1" sx={{ width: "15%" }}>
+                </Grid>
+                <Grid item xs={0} md={0} lg={2} sx={{ display: {xs: 'none', lg: 'block' } }}>
+                    <Typography
+                        variant="subtitle1"
+                        sx={{ textAlign: 'right' }}
+                        >
                         Assignees
                     </Typography>
-                    <Box sx={{ width: "5%" }}></Box>
-                </Stack>
-                <TaskList tasks={tasks}/>
-            </Stack>
-        </>
+                </Grid>
+                <Grid item xs={0} md={1} lg={1} sx={{ display: {xs: 'none', md: 'block' } }}></Grid>
+            </Grid>
+
+            { tasks.length > 0 ? <TaskList tasks={tasks} /> : null }
+        </Box>
     );
 };
 
